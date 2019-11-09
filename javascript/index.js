f1988c6d0dad0590e805b2237e4533f969effd04
jaronow <waronow123@gmail.com>
2019-11-08 18:29:46 -0700

    added text transition and flipped top and bottom text

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,20 @@
 const panels = document.querySelectorAll('.panel')
 panels.forEach(panel => panel.addEventListener('click', toggleOpen))
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive))
 
 function toggleOpen() {
   this.classList.toggle('open')
+}
+
+function toggleActive(event) {
+  if (event.propertyName.includes('flex')) {
+    this.classList.toggle('open-active')
+  }
+}
+
+const inputs = document.querySelectorAll('.controls input')
+inputs.forEach(input => input.addEventListener('change', updateStyle));
+
+function updateStyle() {
+  document.documentElement.style.setProperty(`--${this.name}`, this.value);
 }
